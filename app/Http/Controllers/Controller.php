7321de6595c35283565bad92653c073da79271d8
Nafila Zarka <nafilazarka33@gmail.com>
2023-10-12 11:53:22 +0700

    membuat migration category

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\category;
+use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use PDOException;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    public function index()
+    {
+       
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,5 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::apiResource('/category',CategoryController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
